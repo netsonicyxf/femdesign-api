dc388d0f9f30d93fc20f9784e92b527f885f1562
xRadne <32938476+xRadne@users.noreply.github.com>
2022-01-19 15:45:29 +0100

    :test_tube: Add performance tests
    Model.deserializeFromFilePath

---
+++ FemDesign.Tests/Performance/Model.DeserializeFromStruxml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FemDesign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Reflection;
+
+namespace FemDesign.Tests.Performance
+{
+    [TestClass()]
+    public class ModelDeserializeFromStruxml
+    {
+
+        [TestCategory("Performance"), TestMethod("large model")]
+        public void Large()
+        {
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "OneDrive - StruSoft AB", "Projects", "femdesign-api", "Performance test files", "large_model.struxml");
+            var time = Utils.Time(() => Model.DeserializeFromFilePath(path));
+
+            Assert.IsTrue(time <= TimeSpan.FromSeconds(1.0), $"Time: {time.TotalSeconds:0.##}s");
+        }
+
+        [TestCategory("Performance"), TestMethod("small model")]
+        public void Small()
+        {
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "OneDrive - StruSoft AB", "Projects", "femdesign-api", "Performance test files", "small_model.struxml");
+            var time = Utils.Time(() => Model.DeserializeFromFilePath(path));
+
+            Assert.IsTrue(time <= TimeSpan.FromSeconds(0.01), $"Time: {time.TotalSeconds:0.##}s");
+        }
+    }
+}
---
+++ FemDesign.Tests/Performance/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FemDesign.Tests.Performance
+{
+    public class Utils
+    {
+        public static TimeSpan Time(Action toTime)
+        {
+            var timer = Stopwatch.StartNew();
+            toTime();
+            timer.Stop();
+            return timer.Elapsed;
+        }
+    }
+}
