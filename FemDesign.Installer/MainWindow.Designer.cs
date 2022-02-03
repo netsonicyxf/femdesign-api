06aacb9812a9cb7d32e9ac7da97cf80a3f4e9ce1
xRadne <32938476+xRadne@users.noreply.github.com>
2022-02-03 15:15:23 +0100

    :sparkles: Added FemDesign.Installer application
    Starting point for #37

---
+++ FemDesign.Installer/MainWindow.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace FemDesign.Installer
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RefreshButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.VersionSelector = new System.Windows.Forms.ComboBox();
+            this.DownloadButton = new System.Windows.Forms.Button();
+            this.GrasshopperCheckBox = new System.Windows.Forms.CheckBox();
+            this.DynamoCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.RefreshButton.Location = new System.Drawing.Point(392, 35);
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(21, 23);
+            this.RefreshButton.TabIndex = 0;
+            this.RefreshButton.Text = "⟳";
+            this.RefreshButton.UseVisualStyleBackColor = true;
+            this.RefreshButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(35, 131);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(378, 249);
+            this.textBox1.TabIndex = 1;
+            // 
+            // VersionSelector
+            // 
+            this.VersionSelector.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.VersionSelector.FormattingEnabled = true;
+            this.VersionSelector.Items.AddRange(new object[] {
+            "Item1",
+            "Item2",
+            "Item3"});
+            this.VersionSelector.Location = new System.Drawing.Point(35, 35);
+            this.VersionSelector.Name = "VersionSelector";
+            this.VersionSelector.Size = new System.Drawing.Size(351, 21);
+            this.VersionSelector.TabIndex = 2;
+            this.VersionSelector.SelectedIndexChanged += new System.EventHandler(this.VersionSelector_SelectedIndexChanged);
+            // 
+            // DownloadButton
+            // 
+            this.DownloadButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DownloadButton.Location = new System.Drawing.Point(35, 86);
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(378, 39);
+            this.DownloadButton.TabIndex = 3;
+            this.DownloadButton.Text = "Install";
+            this.DownloadButton.UseVisualStyleBackColor = true;
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
+            // 
+            // GrasshopperCheckBox
+            // 
+            this.GrasshopperCheckBox.AutoSize = true;
+            this.GrasshopperCheckBox.Checked = true;
+            this.GrasshopperCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.GrasshopperCheckBox.Location = new System.Drawing.Point(35, 62);
+            this.GrasshopperCheckBox.Name = "GrasshopperCheckBox";
+            this.GrasshopperCheckBox.Size = new System.Drawing.Size(86, 17);
+            this.GrasshopperCheckBox.TabIndex = 4;
+            this.GrasshopperCheckBox.Text = "Grasshopper";
+            this.GrasshopperCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // DynamoCheckBox
+            // 
+            this.DynamoCheckBox.AutoSize = true;
+            this.DynamoCheckBox.Enabled = false;
+            this.DynamoCheckBox.Location = new System.Drawing.Point(127, 62);
+            this.DynamoCheckBox.Name = "DynamoCheckBox";
+            this.DynamoCheckBox.Size = new System.Drawing.Size(65, 17);
+            this.DynamoCheckBox.TabIndex = 5;
+            this.DynamoCheckBox.Text = "Dynamo";
+            this.DynamoCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 411);
+            this.Controls.Add(this.DynamoCheckBox);
+            this.Controls.Add(this.GrasshopperCheckBox);
+            this.Controls.Add(this.DownloadButton);
+            this.Controls.Add(this.VersionSelector);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.RefreshButton);
+            this.Name = "MainWindow";
+            this.Text = "FEM-Design API installer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RefreshButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox VersionSelector;
+        private System.Windows.Forms.Button DownloadButton;
+        private System.Windows.Forms.CheckBox GrasshopperCheckBox;
+        private System.Windows.Forms.CheckBox DynamoCheckBox;
+    }
+}
+
---
+++ FemDesign.Installer/MainWindow.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using Octokit;
+
+namespace FemDesign.Installer
+{
+    public partial class MainWindow : Form
+    {
+        private GitHubClient GithubClient;
+        private Dictionary<int, Release> Releases = new Dictionary<int, Release>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.GithubClient = new GitHubClient(new ProductHeaderValue("StruSoft-femdesign-api-FemDesign.Installer"));
+
+            UpdateReleaseList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdateReleaseList();
+        }
+
+        private async void UpdateReleaseList()
+        {
+            VersionSelector.Items.Clear();
+            Releases.Clear();
+
+            var releases = await GithubClient.Repository.Release.GetAll("StruSoft", "femdesign-api");
+
+            VersionSelector.Items.AddRange(releases.Select(r => (object)$"{r.TagName} - {r.Name}").ToArray());
+            Releases = releases.ToDictionary(r => r.Id);
+
+            if (VersionSelector.SelectedIndex < 0)
+                VersionSelector.SelectedIndex = 0;
+
+        }
+
+        private async void DownloadButton_Click(object sender, EventArgs e)
+        {
+            var selected = (string)VersionSelector.SelectedItem;
+
+            Release selectedRelease = Releases.Values.First(r => $"{r.TagName} - {r.Name}" == selected);
+
+            // Download FemDesign.Grasshopper
+            ReleaseAsset femdesignGrasshopper = selectedRelease.Assets.FirstOrDefault(a => a.Name == "FemDesign.Grasshopper.zip");
+            if (GrasshopperCheckBox.Checked && femdesignGrasshopper != null)
+            {
+                string GrasshopperInstallDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Grasshopper", "Libraries", "FemDesign");
+                string path = await Download(femdesignGrasshopper.BrowserDownloadUrl, GrasshopperInstallDirectory);
+                UnzipInDirectory(path);
+                textBox1.AppendText($"Installed {femdesignGrasshopper.Name.Replace(".zip", "")} - {selectedRelease.TagName}" + Environment.NewLine);
+            }
+
+            // Download FemDesign.Dynamo
+            ReleaseAsset femdesignDynamo = selectedRelease.Assets.FirstOrDefault(a => a.Name == "FemDesign.Dynamo.zip");
+            if (DynamoCheckBox.Checked && femdesignDynamo != null)
+            {
+                //string DynamoInstallDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Dynamo", , "FemDesign");
+                //string path = await Download(femdesignDynamo.BrowserDownloadUrl, DynamoInstallDirectory);
+                //Unzip(path);
+            }
+        }
+
+        private void VersionSelector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+        }
+
+        public static async Task<string> Download(string url, string directory)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            var bytes = await response.Content.ReadAsByteArrayAsync();
+            var fileName = response.Content.Headers.ContentDisposition.FileName;
+            var path = Path.Combine(directory, fileName);
+            if (File.Exists(path))
+                File.Delete(path);
+
+            using (var stream = new FileStream(path, System.IO.FileMode.CreateNew, FileAccess.Write))
+            {
+                await stream.WriteAsync(bytes, 0, bytes.Length);
+            }
+            return path;
+        }
+
+        public void UnzipInDirectory(string zipPath)
+        {
+            string directory = Path.GetDirectoryName(zipPath);
+
+            // Delete files before extracting
+            using (var zip = ZipFile.OpenRead(zipPath))
+            foreach (var entry in zip.Entries)
+            {
+                var path = Path.Combine(directory, entry.FullName);
+                if (File.Exists(path))
+                    File.Delete(path);
+            }
+            ZipFile.ExtractToDirectory(zipPath, directory, Encoding.UTF8);
+            File.Delete(zipPath);
+        }
+    }
+}
