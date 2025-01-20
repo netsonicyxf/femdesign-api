bd2a6a1981b1589cd2ecac08cd8127813b2d5c38
MP <pellegrino.marco@icloud.com>
2025-01-20 12:38:51 +0100

    🔥 load combination generator from load group

---
+++ FemDesign.Tests/Loads/LoadGroup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FemDesign.Loads;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FemDesign.Loads
+{
+    [TestClass()]
+    public class LoadGroupTests
+    {
+        [TestMethod("Load group serialise/deserialise")]
+        public void LoadGroupTest1()
+        {
+            var model = Model.DeserializeFromFilePath("Loads/Assets/load-group.struxml");
+
+            var loadGroupTable = model.Entities.Loads.LoadGroupTable;
+
+            var customTable = loadGroupTable.CustomTable;
+
+            Assert.IsTrue(loadGroupTable.GeneralLoadGroups[0].ModelLoadGroupPermanent.Load_case.Count == 2);
+            Assert.IsTrue(loadGroupTable.GeneralLoadGroups[1].ModelLoadGroupTemporary.Load_case.Count == 1);
+            Assert.IsTrue(loadGroupTable.GeneralLoadGroups[2].AccidentalLoadGroup.Load_case.Count == 1);
+            Assert.IsTrue(loadGroupTable.GeneralLoadGroups[3].StressLoadGroup.Load_case.Count == 1);
+
+        }
+    }
+}
