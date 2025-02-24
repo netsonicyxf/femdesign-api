05cee9504e461fc7a9e56d5521cb535c61e59dec
MP <pellegrino.marco@icloud.com>
2025-02-24 09:45:00 +0100

    🆙 namespace

--- FemDesign.Tests/Drawing/DimensionLinear.cs
+++ FemDesign.Tests/Drawing/DimensionLinear.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using FemDesign.Geometry;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FemDesign.Geometry;
 
 namespace FemDesign.Drawing
 {
@@ -12,19 +13,19 @@ namespace FemDesign.Drawing
         [TestMethod("Calculate distances of linear dimension")]
         public void Distances()
         {
-            var p0 = new Point3d(0, 0, 0);
-            var p1 = new Point3d(1, 1, 5);
-            var p2 = new Point3d(3, 3, 7);
-            var refPoints = new List<Point3d> { p0, p1, p2 };
+            var p0 = new FemDesign.Geometry.Point3d(0, 0, 0);
+            var p1 = new FemDesign.Geometry.Point3d(1, 1, 5);
+            var p2 = new FemDesign.Geometry.Point3d(3, 3, 7);
+            var refPoints = new List<FemDesign.Geometry.Point3d> { p0, p1, p2 };
             
             // 1
-            var plane = new Plane(Point3d.Origin, new Vector3d(1, 1, 0), new Vector3d(-1, 1, 0));
+            var plane = new Plane(FemDesign.Geometry.Point3d.Origin, new Vector3d(1, 1, 0), new Vector3d(-1, 1, 0));
             var dim = new DimensionLinear(refPoints, plane);
             Assert.IsTrue(Math.Abs(dim.Distances[0] - 1.414214) < 1e-6);
             Assert.IsTrue(Math.Abs(dim.Distances[1] - 2.828427) < 1e-6);
 
             // 2
-            plane = new Plane(Point3d.Origin, new Vector3d(1, 1, 1), new Vector3d(-1, 1, 0));
+            plane = new Plane(FemDesign.Geometry.Point3d.Origin, new Vector3d(1, 1, 1), new Vector3d(-1, 1, 0));
             dim = new DimensionLinear(refPoints, plane);
             Assert.IsTrue(Math.Abs(dim.Distances[0] - 4.041452) < 1e-6);
             Assert.IsTrue(Math.Abs(dim.Distances[1] - 3.464102) < 1e-6);    
