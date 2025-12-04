184772bea6e13ebd6b69ac58e4ab7685db61812c
lorinczandrea <lorincz.andrea@strusoft.hu>
2025-12-04 20:36:01 +0100

    add KeepOpen() to Dispose

--- FemDesign.Grasshopper/Helpers/FemDesignConnectionHub.cs
+++ FemDesign.Grasshopper/Helpers/FemDesignConnectionHub.cs
@@ -37,16 +37,22 @@ namespace FemDesign.Grasshopper
                     catch { /* Swallow here; exceptions are delivered via TaskCompletionSource */ }
                 }
             });
+
             inst.WorkerThread.IsBackground = true;
             inst.WorkerThread.Name = $"FemDesignConnectionHub-Worker-{id}";
-            if (!_instances.TryAdd(id, inst)) throw new InvalidOperationException("Failed to create FemDesign connection instance.");
+
+            if (!_instances.TryAdd(id, inst)) 
+                throw new InvalidOperationException("Failed to create FemDesign connection instance.");
+
             inst.WorkerThread.Start();
             return id;
         }
 
         private static Instance Require(Guid id)
         {
-            if (!_instances.TryGetValue(id, out var inst)) throw new InvalidOperationException("Invalid or disposed FemDesign connection handle.");
+            if (!_instances.TryGetValue(id, out var inst)) 
+                throw new InvalidOperationException("Invalid or disposed FemDesign connection handle.");
+
             return inst;
         }
 
@@ -54,11 +60,13 @@ namespace FemDesign.Grasshopper
         {
             var inst = Require(id);
             var tcs = new TaskCompletionSource<T>();
+
             inst.Queue.Add(() =>
             {
                 try { tcs.SetResult(func(inst.Connection)); }
                 catch (Exception ex) { tcs.SetException(ex); }
             });
+
             return tcs.Task;
         }
 
@@ -66,28 +74,50 @@ namespace FemDesign.Grasshopper
         {
             var inst = Require(id);
             var tcs = new TaskCompletionSource<bool>();
+            
             inst.Queue.Add(() =>
             {
-                try { action(inst.Connection); tcs.SetResult(true); }
+                try 
+                { 
+                    action(inst.Connection); 
+                    tcs.SetResult(true); 
+                }
                 catch (Exception ex) { tcs.SetException(ex); }
             });
+
             return tcs.Task;
         }
 
-        public static Task DisposeAsync(Guid id)
+        public static Task DisposeAsync(Guid id) => DisposeAsync(id, false);
+
+        public static Task DisposeAsync(Guid id, bool detach)
         {
             var tcs = new TaskCompletionSource<bool>();
-            if (!_instances.TryGetValue(id, out var inst)) { tcs.SetResult(true); return tcs.Task; }
+            if (!_instances.TryGetValue(id, out var inst)) 
+            { 
+                tcs.SetResult(true); 
+
+                return tcs.Task; 
+            }
 
             inst.Queue.Add(() =>
             {
-                try { inst.Connection?.Dispose(); inst.Connection = null; }
+                try 
+                { 
+                    if(detach)
+                    {
+                        inst.Connection?.KeepOpen();
+                    }
+                    inst.Connection?.Dispose(); 
+                    inst.Connection = null; 
+                }
                 catch { /* ignore */ }
                 finally { tcs.SetResult(true); }
             });
 
             inst.Queue.CompleteAdding();
             _instances.TryRemove(id, out _);
+
             return tcs.Task;
         }
     }
