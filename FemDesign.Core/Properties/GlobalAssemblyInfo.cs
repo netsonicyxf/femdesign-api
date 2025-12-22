b6e5baf520b6d6e9731dc60c2674d25865fa0964
MP <pellegrino.marco@icloud.com>
2025-12-22 11:47:49 +0100

    ✨  25.0.0

--- FemDesign.Core/Properties/GlobalAssemblyInfo.cs
+++ FemDesign.Core/Properties/GlobalAssemblyInfo.cs
@@ -23,5 +23,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("24.3.0")]
-[assembly: AssemblyFileVersion("24.3.0")]
+[assembly: AssemblyVersion("25.0.0")]
+[assembly: AssemblyFileVersion("25.0.0")]
